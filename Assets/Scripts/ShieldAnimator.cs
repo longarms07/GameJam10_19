baafe7e392fe7293d1735fbb64577f974b890c3d
longarms07 <ecridd@gmail.com>
2019-10-05 13:21:24 -0400

    Started setting up player ship

---
+++ Assets/Scripts/ShieldAnimator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldAnimator : MonoBehaviour
+{
+
+    public int frameRate;
+    public Sprite s0;
+    public Sprite s1;
+    public Sprite s2;
+    public Sprite s3;
+    public bool setActive;
+    private int currentFrame;
+    private SpriteRenderer spriteRenderer;
+    private bool active;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (setActive) SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (active)
+        {
+            if (currentFrame == frameRate * 4) currentFrame = 0;
+            if (currentFrame == 0) spriteRenderer.sprite = s0;
+            if (currentFrame == frameRate * 1) spriteRenderer.sprite = s1;
+            if (currentFrame == frameRate * 2) spriteRenderer.sprite = s2;
+            if (currentFrame == frameRate * 3) spriteRenderer.sprite = s3;
+            currentFrame++;
+        }
+    }
+
+    public void SetActive(bool set)
+    {
+        if (set) currentFrame = 0;
+        active = set;
+    }
+}
