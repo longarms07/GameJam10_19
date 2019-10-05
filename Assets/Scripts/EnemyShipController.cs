d13a6ca4fcd8aaf3ab07b1f5573bb85c249af4c1
Chief Nelson <chief@Chiefs-MacBook-Pro.local>
2019-10-05 17:53:51 -0400

    Made progress towards enemy ship maneuvering

--- Assets/Scripts/EnemyShipController.cs
+++ Assets/Scripts/EnemyShipController.cs
@@ -19,18 +19,15 @@ public class EnemyShipController : MonoBehaviour
         rb2d = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        /*
-            move torwards or away from the player
-         */
         Vector2 target = GameManager.getInstance().player.transform.localPosition;
 
-        Vector2 moveTowards = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        Vector2 offsetFromTarget = new Vector2(transform.position.x, transform.position.y) - target;
 
         //moves away from the player when it gets too close
-        if (Vector2.Distance(transform.position, target) < retreatThreshold) {
+        if (Vector2.Distance(transform.position, target) < retreatThreshold)
+        {
             retreat = true;
         }
         else if (Vector2.Distance(transform.position, target) >= retreatUntil)
@@ -38,43 +35,16 @@ public class EnemyShipController : MonoBehaviour
             retreat = false;
         }
 
-        
-        if (retreat) {
-            moveTowards = Vector2.MoveTowards(transform.position, target, -1 * speed * Time.deltaTime);
-        }
-        
-
-
-        /*
-            face the move direction
-         */
-        Vector2 face = moveTowards - new Vector2(transform.position.x, transform.position.y);
-
-        //get the angle and rotate 
-        float angle = Mathf.Atan2(face.x, face.y) * Mathf.Rad2Deg * -1;
-
-        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-
-        //turn around when nessecary 
         if (retreat)
         {
-            Quaternion.Inverse(rotation);
-            angle += 180 % 360;
-            angle += 0.00f;
+            rb2d.AddForce(-offsetFromTarget * speed);
         }
         else {
-            angle -= 0.00f;
+            rb2d.AddForce(offsetFromTarget * speed);
         }
+        
 
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotateSpeed * Time.deltaTime);
 
-
-        Vector2 newPos = new Vector2(transform.position.x, transform.position.y) + new Vector2(speed * Mathf.Cos(angle * Mathf.Deg2Rad), speed * Mathf.Sin(angle * Mathf.Deg2Rad));
-
-        moveTowards = Vector2.MoveTowards(transform.position, newPos, speed * Time.deltaTime);
-        if (retreat) { moveTowards = Vector2.MoveTowards(transform.position, newPos, -1 * speed * Time.deltaTime); }
-
-        rb2d.MovePosition(moveTowards);
     }
 
 }
