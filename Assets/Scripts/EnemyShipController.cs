4b1482556f932910186d0926d89a1b0447514a8f
Chief Nelson <chief@Chiefs-MacBook-Pro.local>
2019-10-05 10:47:44 -0400

    Fixed enemy ship move towards player

--- Assets/Scripts/EnemyShipController.cs
+++ Assets/Scripts/EnemyShipController.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class EnemyShipController : MonoBehaviour
 {
+    private Rigidbody2D rb2d;
+
     public float speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -17,6 +19,8 @@ public class EnemyShipController : MonoBehaviour
     {
         Vector2 target = GameManager.getInstance().player.transform.localPosition;
 
-        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        Vector2 moveTowards = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        rb2d.MovePosition(moveTowards);
     }
 }
