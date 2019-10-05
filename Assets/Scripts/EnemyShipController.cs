37b9f0e406792a8cc68368bfd79ce0798e341fff
Chief Nelson <chief@Chiefs-MacBook-Pro.local>
2019-10-05 13:05:25 -0400

    Enemy ship reverses when too close to the player
    Has many issues

--- Assets/Scripts/EnemyShipController.cs
+++ Assets/Scripts/EnemyShipController.cs
@@ -8,6 +8,10 @@ public class EnemyShipController : MonoBehaviour
 
     public float speed;
     public float rotateSpeed;
+    public float retreatThreshold;
+    public float retreatUntil;
+
+    private bool retreat;
 
     // Start is called before the first frame update
     void Start()
@@ -18,21 +22,48 @@ public class EnemyShipController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
+            move torwards or away from the player
+         */
         Vector2 target = GameManager.getInstance().player.transform.localPosition;
 
         Vector2 moveTowards = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
 
-        //get the x and y of the vector to face
+        //moves away from the player when it gets too close
+        if (Vector2.Distance(transform.position, target) < retreatThreshold) {
+            retreat = true;
+        }
+        else if (Vector2.Distance(transform.position, target) >= retreatUntil)
+        {
+            retreat = false;
+        }
+
+        if (retreat) {
+            moveTowards = Vector2.MoveTowards(transform.position, target, -1 * speed * Time.deltaTime);
+        }
+
+
+        rb2d.MovePosition(moveTowards);
+
+
+        /*
+            face the move direction
+         */
         Vector2 face = moveTowards - new Vector2(transform.position.x, transform.position.y);
         double y = moveTowards.y - transform.position.y;
 
+        //get the angle and rotate 
         float angle = Mathf.Atan2(face.x, face.y) * Mathf.Rad2Deg * -1;
+
+        //turn around when nessecary 
+        if (retreat) {
+            angle *= -1;
+        }
 
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotateSpeed * Time.deltaTime);
 
-
-        rb2d.MovePosition(moveTowards);
     }
+
 }
