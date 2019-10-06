0ff7af7c153c4c691e239762823f736cdecaa9d5
Chief Nelson <chief@Chiefs-MacBook-Pro.local>
2019-10-06 11:43:38 -0400

    Added explosion on collision for missile

--- Assets/MissileController.cs
+++ Assets/MissileController.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class MissileController : MonoBehaviour
 {
+    public EnemyShipController ship;
+
+    private ParticleSystem explosion;
+    private SpriteRenderer spriteRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        explosion = GetComponent<ParticleSystem>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    // Called automatically on collision
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if((collision.gameObject.layer >= 8 && collision.gameObject.layer <= 11) || collision.gameObject.layer == 0)
+        {
+            Kersplode();
+            Debug.Log(collision.gameObject.layer);
+        }
+    }
+
+    public void Kersplode()
+    {
+
+        spriteRenderer.sprite = null;
+        if (explosion != null)
+        {
+            explosion.Play();
+            Destroy(this.gameObject, explosion.main.duration);
+        }
     }
 }
